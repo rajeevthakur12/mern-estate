f7223303d78fdaf6d1f326ef2c3b79803363ed6f
Your Name <you@example.com>
2024-01-19 23:34:26 +0530

    first api route

--- api/index.js
+++ api/index.js
@@ -1,13 +1,19 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 dotenv.config();
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose
+.connect(process.env.MONGO)
+.then(()=>{
     console.log("Connected to MongoDb");
-}).catch((err)=>{
+})
+.catch((err)=>{
     console.log(err);
 });
 const app=express();
+
+app.use('/api/user',userRouter);
 
 app.listen(3000,()=>{
     console.log("server is running on port 3000");
