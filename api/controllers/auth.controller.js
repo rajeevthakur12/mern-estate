8920421f5b04c67ad8c9bf7319d9d2d383f248fe
Your Name <you@example.com>
2024-01-22 15:46:00 +0530

    complete signup ui page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcrypt';
 import User from '../models/user.model.js';
-export const signup=async(req,res)=>{
+import {errorHandler} from '../utils/error.js';
+export const signup=async(req,res,next)=>{
     const {username,email,password}= req.body;
     const hashedPassword=bcrypt.hashSync(password,10);
     const newUser=new User({username,email,password:hashedPassword});
@@ -10,6 +11,6 @@ export const signup=async(req,res)=>{
         res.status(201).json("user created successfully");
     }
     catch(error){
-        res.status(500).json(error.message);
+        next(error);
     }
 };
--- api/index.js
+++ api/index.js
@@ -18,6 +18,17 @@ app.use(express.json());
 app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);
 
+//middleware
+app.use((err,req,res,next)=>{
+    const statusCode=err.statusCode || 500;
+    const message=err.message || "internal server error";
+    return res.status(statusCode).json({
+        success:true,
+        statusCode,
+        message,
+    });
+});
+
 app.listen(3000,()=>{
     console.log("server is running on port 3000");
 });
