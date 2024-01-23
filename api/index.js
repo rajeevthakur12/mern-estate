499b5a3c82c0a41e671aa84b49610deca5926373
Your Name <you@example.com>
2024-01-23 16:20:21 +0530

    complete signin page functionality

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ app.use((err,req,res,next)=>{
     const statusCode=err.statusCode || 500;
     const message=err.message || "internal server error";
     return res.status(statusCode).json({
-        success:true,
+        success:false,
         statusCode,
         message,
     });
