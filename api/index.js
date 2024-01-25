2ac771e97b7aa766a1a6e0d87bae11c81fd3bae4
Your Name <you@example.com>
2024-01-25 15:53:34 +0530

    update image functionality added

--- api/index.js
+++ api/index.js
@@ -1,35 +1,35 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js';
-import cors from 'cors';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import cors from "cors";
 dotenv.config();
 mongoose
-.connect(process.env.MONGO)
-.then(()=>{
+  .connect(process.env.MONGO)
+  .then(() => {
     console.log("Connected to MongoDb");
-})
-.catch((err)=>{
+  })
+  .catch((err) => {
     console.log(err);
-});
-const app=express();
+  });
+const app = express();
 app.use(express.json());
 
-app.use('/api/user',userRouter);
-app.use('/api/auth',authRouter);
-
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use(cors());
 //middleware
-app.use((err,req,res,next)=>{
-    const statusCode=err.statusCode || 500;
-    const message=err.message || "internal server error";
-    return res.status(statusCode).json({
-        success:false,
-        statusCode,
-        message,
-    });
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "internal server error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
 
-app.listen(3000,()=>{
-    console.log("server is running on port 3000");
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
 });
