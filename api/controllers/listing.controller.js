0c4decce6134b6eab2dc76349d86e9d5fba739ef
Your Name <you@example.com>
2024-02-03 17:06:58 +0530

    search api route completed

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -55,3 +55,42 @@ export const getListing = async (req, res, next) => {
     next(error);
   }
 };
+export const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    let offer = req.query.offer;
+    if (offer === undefined && offer === "false") {
+      offer = { $in: [false, true] };
+    }
+    let furnished = req.query.furnished;
+    if (furnished === undefined && furnished === "false") {
+      furnished = { $in: [false, true] };
+    }
+    let parking = req.query.parking;
+    if (parking === undefined && parking === "false") {
+      parking = { $in: [false, true] };
+    }
+    const type = req.query.type;
+    if (type === undefined || type === "all") {
+      type = { $in: [false, true] };
+    }
+    const sort = req.query.sort || createdAt;
+    const searchTerm = req.query.searchTerm || "";
+    const order = req.query.order || "desc";
+
+    const listings = await Listing.find({
+      name: { $regex: searchTerm, $options: "i" },
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+      .sort({ [sort]: order })
+      .limit(limit)
+      .skip(startIndex);
+    res.send(200).json(listings);
+  } catch (error) {
+    next(error);
+  }
+};
