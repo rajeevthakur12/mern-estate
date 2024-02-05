d57add23feeb6b8d269cf7fe201493dfa545d02e
Your Name <you@example.com>
2024-02-05 22:12:00 +0530

    project completed

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -15,13 +16,19 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+const __dirname = path.resolve();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
 app.use(cors());
+app.use(express.static(path.join(__dirname, "/client/dest")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join("client", "dest", "index.html"));
+});
 //middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
